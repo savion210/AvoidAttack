2a3bb62eae159f389e4a5301fb85d29f0c243554
PinedaJeremy <pinedajeremy16@gmail.com>
2018-12-18 10:08:25 -0500

    Made Game manager
    Not complete

---
+++ AvoidAttack/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	[Serializedfield] 
+	public bool SurvivalMode;
+
+    [Serializedfield]
+    public bool Levels; // you can change the name of this mode to anything you want, this is supposed to be the game mode thats beatable.
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
